2b14f5168bd832bf2cd9ef871c180fefe21ea2bd
Alwin Gieselmann <alwin.gieselmann@ncl.ac.uk>
2018-12-04 15:30:07 +0000

    updating things here as well

--- nlx_control_start.m
+++ nlx_control_start.m
@@ -2,20 +2,44 @@ function nlx_control_start
 
 % starts the NLX_CONTROL environment
 
-%% puts subdirectories on the matlab path
 MainDir = fileparts(which('nlx_control_start'));
-addpath( ...
-    MainDir, ...
-    [MainDir '\SysFun'], ...
-    [MainDir '\SysFun\misc'], ...
-    [MainDir '\SysFun\NetCom']);
+
+%% puts subdirectories on the matlab path
+
+addpath(MainDir);
+addpath(fullfile(MainDir,'SysFun'));
 addpath(nlx_control_getSettingsDir);
+addpath(fullfile(MainDir,'SysFun','NetCom'));
+
+addpath(fullfile(MainDir,'SysFun','misc'));
+
+al_spk_ObjPath = which('al_spk');
+if isempty(al_spk_ObjPath)
+    addpath(fullfile(MainDir,'SysFun','misc'));
+else
+    % assume that all necessary libraries are existent
+    fprintf('Detected @al_spk in -> %s\n',fileparts(al_spk_ObjPath));
+end
+
 
 %% start the GUI
 nlx_control_gui;
 
 % set GUI menues
 H = nlx_control_gui_AnalyseMenu;
-H = nlx_control_gui_SEChannelMenu;
-H = nlx_control_gui_CSCChannelMenu;
+
+ChannelNrs    = [1:16];
+ChannelLabel  = {'e1SE' 'e2SE'};
+ClusterNrs    = [0];
+
+% ChannelNrs = [1:16];
+% ChannelLabel  = {'e1SE' 'e2SE' 'e3SE'};
+% ClusterNrs = [0];
+
+% ChannelNrs = [1:2];
+% ChannelLabel  = {'SE'};
+% ClusterNrs = [0];
+
+
+H = nlx_control_gui_SEChannelMenu(ChannelLabel,ChannelNrs,ClusterNrs);
 
